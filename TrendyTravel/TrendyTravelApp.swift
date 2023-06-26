bc8d87284c774cad867dce726eb657303a1fd633
julie collazos <juliecollazos@hotmail.com>
2023-06-26 11:39:53 +0200

    Initial Commit

---
+++ TrendyTravel/TrendyTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendyTravelApp.swift
+//  TrendyTravel
+//
+//  Created by Julie Collazos on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrendyTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
