920d443b1ddad0c64c885e304d88e405749650fe
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-07-17 10:47:15 +0200

    feat: Update project layout

---
+++ TrendyTravel/View Models/TrendyTravelViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TrendyTravelViewModel.swift
+//  TrendyTravel
+//
+//  Created by Yann Christophe Maertens on 17/07/2023.
+//
+
+import SwiftUI
+
+class TrendyTravelViewModel: ObservableObject {
+    
+}
