f2e8540dcefabd2a2e2d2b611d49edaf63cfa2d7
NatCanCode <nathalie.piat1@gmail.com>
2023-07-18 15:34:48 +0200

    Fix further instances of "image" typo

--- TrendyTravel/Views/Destinations/PopularDestinationsView.swift
+++ TrendyTravel/Views/Destinations/PopularDestinationsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct PopularDestinationsView: View {
     let destinations: [Destination] = [
-        .init(id: 1, country: "France", city: "Paris", inageName: "eiffel_tower", latitude: 48.859565, longitude: 2.353235)
+        .init(id: 1, country: "France", city: "Paris", imageName: "eiffel_tower", latitude: 48.859565, longitude: 2.353235)
         ]
     var body: some View {
         VStack {
@@ -43,7 +43,7 @@ struct PopularDestinationsView: View {
 struct PopularDestinationsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            PopularDestinationsDetailsView(destination: .init(id: 1, country: "France", city: "Paris", inageName: "eiffel_tower", latitude: 48.859565, longitude: 2.353235))
+            PopularDestinationsDetailsView(destination: .init(id: 1, country: "France", city: "Paris", imageName: "eiffel_tower", latitude: 48.859565, longitude: 2.353235))
         }
     }
 }
