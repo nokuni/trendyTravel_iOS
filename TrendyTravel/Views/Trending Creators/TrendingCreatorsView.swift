2eeac6fb98acbc91b9dfedf310327bcdec57dca3
NatCanCode <nathalie.piat1@gmail.com>
2023-07-21 15:13:42 +0200

    Remove black foregroundColor

--- TrendyTravel/Views/Trending Creators/TrendingCreatorsView.swift
+++ TrendyTravel/Views/Trending Creators/TrendingCreatorsView.swift
@@ -14,11 +14,12 @@ struct TrendingCreatorsListView: View {
             HStack {
                 Text(L10n.TrendingCreatorsListView.title)
                     .font(.system(size: 14, weight: .semibold))
-                    .foregroundColor(.black)
+
                 Spacer()
+
                 Text(L10n.TrendingCreatorsListView.see_all)
                     .font(.system(size: 12, weight: .semibold))
-                    .foregroundColor(.black)
+
             }
             .padding(.top)
             .onAppear { userVM.fetchUsers() }
@@ -33,7 +34,6 @@ struct TrendingCreatorsListView: View {
                         }
                     }
                 }
-                .foregroundColor(.black)
                 .padding(.horizontal)
                 .padding(.bottom)
             }
